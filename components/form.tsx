b04acc574befefd8890c022ad8747264714b2cfd
s0vers <sovers.tonmoy.pandey@g.bracu.ac.bd>
2024-01-18 13:14:52 +0600

    design fixes

--- components/form.tsx
+++ components/form.tsx
@@ -716,28 +716,21 @@ export default function Form() {
 
       {/* Navigation */}
       <div className=' mt-52 pt-5'>
-        <div className='flex justify-between'>
+        <div className='flex items-end justify-between align-baseline'>
           <button
             type='button'
             onClick={prev}
-            disabled={currentStep === 0}
-            className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
+            hidden={currentStep === 0 || currentStep === steps.length - 1}
+            style={{
+              background: 'linear-gradient(128deg, #41DDFF 0%, #7D2DFF 100%)'
+            }}
+            className='rounded-3xl px-10 py-3 text-sm font-medium text-white'
           >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth='1.5'
-              stroke='currentColor'
-              className='h-6 w-6'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M15.75 19.5L8.25 12l7.5-7.5'
-              />
-            </svg>
+            Previous
           </button>
+          <p>
+            {currentStep + 1} of {steps.length}
+          </p>
           <button
             type='button'
             onClick={next}
